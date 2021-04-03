49d61990c917fe1c7d0f721d34cbb74b0551d6d6
Cong Nguyen <rampadc@users.noreply.github.com>
2021-04-04 02:39:28 +1000

    Changed cameras list layout

--- src/pages/cameras.js
+++ src/pages/cameras.js
@@ -1,52 +1,59 @@
-import { DataTable, Table, TableBody, TableCell, TableContainer, TableHead, TableHeader, TableRow } from 'carbon-components-react';
-import React from 'react';
-import Layout from '../components/layout';
+import {
+  Button,
+  CodeSnippet,
+  Column,
+  FormGroup,
+  Grid,
+  RadioButtonGroup,
+  Row,
+} from "carbon-components-react";
+import { RadioButton } from "carbon-components-react/lib/components/radiobutton/radiobutton";
+import React from "react";
+import Layout from "../components/layout";
 
 const CamerasPage = () => {
-  let headerData = [{
-    header: 'Name',
-    key: 'name'
-  }, {
-    header: 'Status',
-    key: 'status'
-  }];
-  let rowsData = [{
-    id: 'id',
-    name: 'Camera 1',
-    status: 'Active'
-  }];
-
   return (
     <Layout>
-      <DataTable rows={rowsData} headers={headerData}>
-        {({ rows, headers, getHeaderProps, getTableProps }) => {
-          return (
-          <TableContainer title="Cameras" description="All the goodness ">
-            <Table {...getTableProps()}>
-              <TableHead>
-                <TableRow>
-                  {headers.map((header) => (
-                    <TableHeader {...getHeaderProps({ header })}>
-                      {header.header}
-                    </TableHeader>
-                  ))}
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <TableRow key={row.id}>
-                    {row.cells.map((cell) => (
-                      <TableCell key={cell.id}>{cell.value}</TableCell>
-                    ))}
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-        )}}
-      </DataTable>
+      
+      <Grid narrow style={{ marginLeft: "unset", marginRight: "unset" }}>
+        <Row>
+          <Column lg={4}>
+          <h4 style={{ marginBottom: "1.5rem" }}>Available cameras</h4>
+          <FormGroup
+  legendText="Select a camera below to preview its properties"
+>
+  <RadioButtonGroup
+    defaultSelected="default-selected"
+    legend="Group Legend"
+    name="radio-button-group"
+    valueSelected="default-selected"
+   orientation='vertical'>
+    <RadioButton
+      id="radio-1"
+      labelText="Radio button label"
+      value="standard"
+    />
+    <RadioButton
+      id="radio-2"
+      labelText="Radio button label"
+      value="default-selected"
+    />
+    <RadioButton
+      id="radio-3"
+      labelText="Radio button label"
+      value="disabled"
+    />
+  </RadioButtonGroup>
+</FormGroup>
+<Button>Switch</Button>
+            </Column>
+          <Column lg={12}>
+            <CodeSnippet type="multi"></CodeSnippet>
+          </Column>
+        </Row>
+      </Grid>
     </Layout>
-  )
+  );
 };
 
 export default CamerasPage;
